4c1b9a7b6f3c5891f7b0ef2ad40d913104d6014b
FakeApollo <hin26@outlook.com>
2026-04-17 12:40:32 +0600

    Added the file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CD-Lab-Work
+## CD-Lab-Work
 This program splits a sentence into tokens and classifies each as a keyword, identifier, literal, operator, or separator.
---
+++ tokenizer.py
@@ -0,0 +1,23 @@
+import re
+
+def tokenize_sentence(sentence):
+    keywords = {"is", "and", "int"}
+    
+    tokens = re.findall(r'[a-zA-Z]+|\d+|[+=;]', sentence)
+
+    for token in tokens:
+        if token.isdigit():
+            print(f"{token} -> Literal")
+        elif token in keywords:
+            print(f"{token} -> Keyword")
+        elif token.isalpha():
+            print(f"{token} -> Identifier")
+        elif token in "+=":
+            print(f"{token} -> Operator")
+        elif token == ";":
+            print(f"{token} -> Separator")
+
+
+
+sentence = "this is a int sum = 10; and a + b = 20"
+tokenize_sentence(sentence)
